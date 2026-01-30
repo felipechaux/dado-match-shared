2890dce0bad31b6ff173af4775fccdb8e0443d35
felipe chaux <felipechauxx@misena.edu.co>
2026-01-30 03:15:07 -0500

    config changes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,14 +2,14 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
     repositories {
-        google()
+        gradlePluginPortal()
         mavenCentral()
+        google()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         maven("https://maven.pkg.jetbrains.space/kotlin/p/wasm/experimental")
         maven("https://androidx.dev/storage/compose-compiler/repository")
-        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
+
         maven { url = uri("https://jitpack.io") }
-        gradlePluginPortal()
     }
 }
 
--- shared/src/iosMain/kotlin/com/dadomatch/shared/MainViewController.kt
+++ shared/src/iosMain/kotlin/com/dadomatch/shared/MainViewController.kt
@@ -1,8 +1,14 @@
+
 package com.dadomatch.shared
 
 import androidx.compose.ui.window.ComposeUIViewController
 import com.dadomatch.shared.presentation.ui.navigation.AppNavigation
 
-fun MainViewController() = ComposeUIViewController {
+fun MainViewController() = ComposeUIViewController(
+    configure = {
+        enforceStrictPlistSanityCheck = false
+    }
+) {
     AppNavigation()
 }
+
